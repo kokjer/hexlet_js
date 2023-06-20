ad62091257a39c37d214f0ff3609fb3d9aeb4616
kokjer <kokjer.faustin14@gmail.com>
2023-06-20 12:26:35 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+import _ from "lodash";
+import React from "react";
+
+console.log(_.capitalize("hello"));
+console.log(_.last(['one', 'two']));
+
+const person = (name, age) => {
+    return `Hello ${name}, Age: ${age}`
+}
